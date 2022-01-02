e43858fe1e91987f7730b5e13def7f701c6823e2
Victor Sergienko <singalen@gmail.com>
2022-01-02 14:43:06 -0800

    walkie: added Drake spritesheet.

---
+++ assets/uLPC-drake.tsx
@@ -0,0 +1,67 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="uLPC-drake" tilewidth="48" tileheight="48" spacing="16" margin="8" tilecount="273" columns="13">
+ <image source="uLPC-drake.png" trans="000000" width="832" height="1344"/>
+ <tile id="0">
+  <properties>
+   <property name="name" value="hail-n"/>
+  </properties>
+  <animation>
+   <frame tileid="0" duration="200"/>
+   <frame tileid="1" duration="200"/>
+   <frame tileid="2" duration="200"/>
+   <frame tileid="3" duration="200"/>
+   <frame tileid="4" duration="200"/>
+   <frame tileid="5" duration="200"/>
+   <frame tileid="6" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="13">
+  <animation>
+   <frame tileid="13" duration="200"/>
+   <frame tileid="14" duration="200"/>
+   <frame tileid="15" duration="200"/>
+   <frame tileid="16" duration="200"/>
+   <frame tileid="17" duration="200"/>
+   <frame tileid="18" duration="200"/>
+   <frame tileid="19" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="52">
+  <animation>
+   <frame tileid="52" duration="60"/>
+   <frame tileid="53" duration="60"/>
+   <frame tileid="54" duration="60"/>
+   <frame tileid="55" duration="60"/>
+   <frame tileid="56" duration="60"/>
+   <frame tileid="57" duration="60"/>
+   <frame tileid="58" duration="60"/>
+   <frame tileid="59" duration="60"/>
+  </animation>
+ </tile>
+ <tile id="130">
+  <properties>
+   <property name="name" value="walk-s"/>
+  </properties>
+  <animation>
+   <frame tileid="130" duration="60"/>
+   <frame tileid="131" duration="60"/>
+   <frame tileid="132" duration="60"/>
+   <frame tileid="133" duration="60"/>
+   <frame tileid="134" duration="60"/>
+   <frame tileid="135" duration="60"/>
+   <frame tileid="136" duration="60"/>
+   <frame tileid="137" duration="60"/>
+   <frame tileid="138" duration="60"/>
+  </animation>
+ </tile>
+ <tile id="260">
+  <animation>
+   <frame tileid="260" duration="120"/>
+   <frame tileid="261" duration="120"/>
+   <frame tileid="262" duration="120"/>
+   <frame tileid="263" duration="120"/>
+   <frame tileid="264" duration="120"/>
+   <frame tileid="265" duration="400"/>
+  </animation>
+ </tile>
+</tileset>
