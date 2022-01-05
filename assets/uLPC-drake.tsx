0129e70d22f3baa0d9252fe1ccfa20617a80ca14
Victor Sergienko <singalen@gmail.com>
2022-01-05 01:15:32 -0800

    walkie: all drake walking animations, transparent color

--- assets/uLPC-drake.tsx
+++ assets/uLPC-drake.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="uLPC-drake" tilewidth="48" tileheight="48" spacing="16" margin="8" tilecount="273" columns="13">
- <image source="uLPC-drake.png" trans="000000" width="832" height="1344"/>
+ <image source="uLPC-drake.png" trans="ffffff" width="832" height="1344"/>
  <tile id="0">
   <properties>
    <property name="name" value="hail-n"/>
@@ -16,6 +16,9 @@
   </animation>
  </tile>
  <tile id="13">
+  <properties>
+   <property name="name" value="hail-w"/>
+  </properties>
   <animation>
    <frame tileid="13" duration="200"/>
    <frame tileid="14" duration="200"/>
@@ -38,6 +41,38 @@
    <frame tileid="59" duration="60"/>
   </animation>
  </tile>
+ <tile id="104">
+  <properties>
+   <property name="name" value="walk-n"/>
+  </properties>
+  <animation>
+   <frame tileid="104" duration="50"/>
+   <frame tileid="105" duration="50"/>
+   <frame tileid="106" duration="50"/>
+   <frame tileid="107" duration="50"/>
+   <frame tileid="108" duration="50"/>
+   <frame tileid="109" duration="50"/>
+   <frame tileid="110" duration="50"/>
+   <frame tileid="111" duration="50"/>
+   <frame tileid="112" duration="50"/>
+  </animation>
+ </tile>
+ <tile id="117">
+  <properties>
+   <property name="name" value="walk-w"/>
+  </properties>
+  <animation>
+   <frame tileid="117" duration="50"/>
+   <frame tileid="118" duration="50"/>
+   <frame tileid="119" duration="50"/>
+   <frame tileid="120" duration="50"/>
+   <frame tileid="121" duration="50"/>
+   <frame tileid="122" duration="50"/>
+   <frame tileid="123" duration="50"/>
+   <frame tileid="124" duration="50"/>
+   <frame tileid="125" duration="50"/>
+  </animation>
+ </tile>
  <tile id="130">
   <properties>
    <property name="name" value="walk-s"/>
@@ -52,6 +87,22 @@
    <frame tileid="136" duration="60"/>
    <frame tileid="137" duration="60"/>
    <frame tileid="138" duration="60"/>
+  </animation>
+ </tile>
+ <tile id="143">
+  <properties>
+   <property name="name" value="walk-e"/>
+  </properties>
+  <animation>
+   <frame tileid="143" duration="50"/>
+   <frame tileid="144" duration="50"/>
+   <frame tileid="145" duration="50"/>
+   <frame tileid="146" duration="50"/>
+   <frame tileid="147" duration="50"/>
+   <frame tileid="148" duration="50"/>
+   <frame tileid="149" duration="50"/>
+   <frame tileid="150" duration="50"/>
+   <frame tileid="151" duration="50"/>
   </animation>
  </tile>
  <tile id="260">
