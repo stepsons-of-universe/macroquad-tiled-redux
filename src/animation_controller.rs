c9f441560650d3f92321199f41189b9b91c933ba
sor-ca <mariagrandova@gmail.com>
2022-01-28 06:50:16 +0200

    cosmetics. Add OutputFrame -> need to modify walkie/main.rs.Without modification of tests

--- src/animation_controller.rs
+++ src/animation_controller.rs
@@ -4,6 +4,16 @@ use tiled::animation;
 use tiled::properties::PropertyValue;
 use tiled::tileset::Tileset;
 
+pub struct OutputFrame {
+    tile_id: u32,
+    /// A point the current animation was started at.
+    start_position: (f32, f32),
+    /// When the current animation was started.
+    start_time: Instant,
+    /// Movement relative to `origin`.
+    offset: (f32, f32),
+}
+
 #[derive(Clone, Copy, Debug)]
 pub struct AnimationFrame {
     pub tile_id: u32,
@@ -101,7 +111,7 @@ impl AnimationInstance {
 
     // Methods are typically named as verbs, esp if they "mutate" (change) object state.
     // This should better be named "compres".
-    pub fn compressed(&mut self, current_time: Instant) {
+    pub fn compress(&mut self, current_time: Instant) {
             let frames = self.frames.clone();
             let mut new_frames: Vec<AnimationFrame> = vec![];
             // Instant implements Copy (can be copied byte-by-byte), so no need to call clone() on it.
@@ -122,6 +132,12 @@ impl AnimationInstance {
             }
             let new_duration = new_frames.iter().map(|it| it.duration.as_ticks() as u64).sum();
             let k = (self.duration.as_ticks() as u64 * self.max_compression as u64 / (new_duration * 100)) as f32;
+            let new_movement = ((self.movement.0 as f32 / k), (self.movement.1 as f32 / k));
+            //self.frame_start = self.frame_start + (self.duration - Duration::from_ticks((new_duration as f32 * k) as u64));
+            //или так
+            self.frame_start = new_start;
+            //или так
+            //self.frame_start = current_time;
             self.frames = new_frames;
             self.duration = Duration::from_ticks(new_duration);
             self.movement = new_movement;
@@ -162,22 +178,28 @@ impl AnimationController {
     /// Returns (frame_id, (x, y)) for the given time moment, if there is
     /// a frame to show, otherwise None.
     /// Only goes down to current or next frame.
-    pub fn get_frame(&self, time: Instant) -> Option<(u32, (f32, f32))> {
+    pub fn get_frame(&self, time: Instant) -> Option<OutputFrame> {
         match self.animations.get(0) {
             Some(i) => {
                 let instance = i;
                 let tile_id = AnimationController::get_tile_id(time, instance);
                 let position = AnimationController::get_position(time, instance);
-                let frame:(u32, (f32, f32)) = (tile_id, position);
+                //let frame:(u32, (f32, f32)) = (tile_id, position);
+                let frame = OutputFrame {
+                    tile_id: tile_id,
+                    start_position: instance.start_position,
+                    start_time: instance.frame_start,
+                    offset: position,
+                };
                 return Some(frame);
             }
             None => None
         }
     }
 
-    pub fn add_animation(&mut self, start_time: Instant, template: &AnimationTemplate, movement: (f32, f32)) {
+    pub fn add_animation(&mut self, start_time: Instant, template: &AnimationTemplate, movement: (f32, f32), start_position: (f32, f32)) {
         let mut new_start_time = start_time;
-        let mut new_start_position: (f32, f32) = (0.0, 0.0);
+        let mut new_start_position = start_position;
         if self.animations.len() != 0 {
             let i = self.animations.last().unwrap();
             new_start_time = i.frame_start + i.duration;
@@ -191,11 +213,11 @@ impl AnimationController {
     // (encapsulation principle, AKA "abstraction layers" AKA low coupling principle).
     // Thus, it's better to make this fn private (or pub(crate), for testing) and call
     // it from add_animation() as needed.
-    pub fn add_animation_with_compression(&mut self, start_time: Instant, template: &AnimationTemplate, movement: (f32, f32)) {
+    pub fn add_animation_with_compression(&mut self, start_time: Instant, template: &AnimationTemplate, movement: (f32, f32), start_position: (f32, f32)) {
         let mut new_start_time = start_time;
-        let mut new_start_position: (f32, f32) = (0.0, 0.0);
+        let mut new_start_position = start_position;
         if self.animations.len() != 0 {
-            self.get_compressed(start_time);
+            self.compress(start_time);
             let i = self.animations.last().unwrap();
             new_start_time = i.frame_start + i.duration;
             new_start_position = (i.start_position.0 + i.movement.0 as f32, i.start_position.1 + i.movement.1 as f32)
@@ -207,13 +229,13 @@ impl AnimationController {
     // Again, this mutates self, so need to be named with a verb.
     // "get_xxx" is traditional name for read-only methods that only return a value of object's property.
     // The "compress" name looks fitting here too.
-    pub fn get_compressed(&mut self, time: Instant) {
+    pub fn compress(&mut self, time: Instant) {
             let mut animations = self.animations.clone();
             // "i" is a traditional name for index, and only index. In most other cases,
             // single-letter names are discouraged. At very least, let's make it "ai".
-            for i in &mut animations {
-                if !i.is_compressed {
-                    i.compressed(time);
+            for a in &mut animations {
+                if !a.is_compressed {
+                    a.compress(time);
                 }
             }
             self.animations = animations;
@@ -240,8 +262,10 @@ impl AnimationController {
         let start_time = instance.frame_start;
         let duration = (finish_time - start_time).as_ticks() as f32;
         let total_duration = instance.duration.as_ticks() as f32;
-        let x = start_position.0 as f32 + ((movement.0 as f32 * duration) / total_duration) as f32;
-        let y = start_position.1 as f32 + ((movement.1 as f32 * duration) / total_duration) as f32;
+        //let x = start_position.0 as f32 + ((movement.0 as f32 * duration) / total_duration) as f32;
+        //let y = start_position.1 as f32 + ((movement.1 as f32 * duration) / total_duration) as f32;
+        let x = movement.0 * duration / total_duration;
+        let y = movement.1 * duration / total_duration;
         (x.round(), y.round())
     }
 }
